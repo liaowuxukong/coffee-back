96fa220914fdfa13e01d2035c7cd88583c9842a2
lucas <liaowuxukong@163.com>
2018-01-28 11:28:12 +0800

    fix

--- src/main/java/com/yizhen/coffee/web/controller/WeChatController.java
+++ src/main/java/com/yizhen/coffee/web/controller/WeChatController.java
@@ -45,7 +45,7 @@ public class WeChatController {
         WxPayData result = new WxPayData();
         try {
             //商户订单号
-            String outTradeNo = UUID.randomUUID().toString(); //WxPayUtil.getOrderFormNumber();
+            String outTradeNo = UUID.randomUUID().toString().substring(0,20); //WxPayUtil.getOrderFormNumber();
             //商品价格
             String totalFee = "1";
             //客户端ip
